4db823a589bbd0e6989b55245f84ed089c89fa90
eslem <j.eslem03@gmail.com>
2014-12-29 12:07:28 -0800

    opacity trackBar

--- YLDP/Player.Designer.vb
+++ YLDP/Player.Designer.vb
@@ -22,86 +22,52 @@ Partial Class YoutubeList
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
         Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
-        Me.FlowLayoutPanel1.SuspendLayout()
-        Me.SuspendLayout()
-        '
-        'Button1
-        '
-        Me.Button1.BackColor = System.Drawing.Color.Transparent
-        Me.Button1.Dock = System.Windows.Forms.DockStyle.Right
-        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button1.ForeColor = System.Drawing.Color.Transparent
-        Me.Button1.Location = New System.Drawing.Point(491, 3)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(65, 25)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Metal"
-        Me.Button1.UseVisualStyleBackColor = False
-        '
-        'Button2
-        '
-        Me.Button2.AutoSize = True
-        Me.Button2.BackColor = System.Drawing.Color.Transparent
-        Me.Button2.Dock = System.Windows.Forms.DockStyle.Right
-        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
-        Me.Button2.ForeColor = System.Drawing.Color.Transparent
-        Me.Button2.Location = New System.Drawing.Point(430, 3)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(55, 25)
-        Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Clasical"
-        Me.Button2.UseVisualStyleBackColor = False
-        '
-        'FlowLayoutPanel1
-        '
-        Me.FlowLayoutPanel1.BackColor = System.Drawing.Color.Transparent
-        Me.FlowLayoutPanel1.Controls.Add(Me.Button1)
-        Me.FlowLayoutPanel1.Controls.Add(Me.Button2)
-        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom
-        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft
-        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 284)
-        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
-        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(559, 31)
-        Me.FlowLayoutPanel1.TabIndex = 3
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        CType(Me.TrackBar1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
         '
         'WebBrowser1
         '
-        Me.WebBrowser1.AllowWebBrowserDrop = False
+        Me.WebBrowser1.AllowWebBrowserDrop = false
         Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
         Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.ScrollBarsEnabled = False
-        Me.WebBrowser1.Size = New System.Drawing.Size(559, 284)
+        Me.WebBrowser1.ScrollBarsEnabled = false
+        Me.WebBrowser1.Size = New System.Drawing.Size(559, 360)
         Me.WebBrowser1.TabIndex = 4
+        Me.WebBrowser1.Url = New System.Uri("https://www.youtube.com/", System.UriKind.Absolute)
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TrackBar1.AutoSize = False
+        Me.TrackBar1.Location = New System.Drawing.Point(406, 333)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(153, 27)
+        Me.TrackBar1.TabIndex = 5
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.None
+        Me.TrackBar1.Value = 7
         '
         'YoutubeList
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.ClientSize = New System.Drawing.Size(559, 315)
+        Me.ClientSize = New System.Drawing.Size(559, 360)
+        Me.Controls.Add(Me.TrackBar1)
         Me.Controls.Add(Me.WebBrowser1)
-        Me.Controls.Add(Me.FlowLayoutPanel1)
         Me.Name = "YoutubeList"
         Me.Opacity = 0.75R
         Me.ShowIcon = False
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
         Me.Text = "Youtube Desktop Player"
-        Me.FlowLayoutPanel1.ResumeLayout(False)
-        Me.FlowLayoutPanel1.PerformLayout()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
-    End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents Button2 As System.Windows.Forms.Button
-    Friend WithEvents FlowLayoutPanel1 As System.Windows.Forms.FlowLayoutPanel
+End Sub
     Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents TrackBar1 As System.Windows.Forms.TrackBar
 
 End Class
--- YLDP/Player.vb
+++ YLDP/Player.vb
@@ -1,7 +1,7 @@
 ﻿Public Class YoutubeList
 
     Private metalPlaylist = "http://www.youtube.com/embed/videoseries?list=PLjO7Bux5L_yP-K0ZiL3h9nUzeWK-Ps-CD"
-    Private clasiclaPlaylist = "http://www.youtube.com/embed/videoseries?list=PL8FABEAD8F2BDF0E7"
+    Private clasiclaPlaylist = "https://www.youtube.com/"
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.TopMost = True
@@ -20,16 +20,22 @@
 
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         Dim url As New System.Uri(metalPlaylist)
         WebBrowser1.Navigate(url)
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
         Dim url As New System.Uri(clasiclaPlaylist)
         WebBrowser1.Url = url
         WebBrowser1.Navigate(url)
 
+    End Sub
+
+    Private Sub TrackBar1_Scroll_1(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        If TrackBar1.Value > 0 Then
+            Me.Opacity = TrackBar1.Value / 10
+        End If
     End Sub
 End Class
